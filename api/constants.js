3771cc44a423631a3ffca684011f0c9bdcbe1826
fnanni-0 <61805270+fnanni-0@users.noreply.github.com>
2023-03-22 01:07:07 -0300

    Update gnosis chain rpc

--- api/constants.js
+++ api/constants.js
@@ -1,5 +1,5 @@
 const PK = ''
-const PROVIDER_URL = 'https://rpc.xdaichain.com/'
+const PROVIDER_URL = 'https://rpc.gnosischain.com/'
 const GAS_LIMIT = '3000000'
 const GAS_PRICE = '2000000000' // 2 gwei
 
